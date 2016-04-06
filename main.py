22819f4e49efc1cae90e85e0ae64dd756f485141
Henry Fisher <henry.wfisher@gmail.com>
2016-04-06 10:56:26 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# pip install metaphone
+from metaphone import doublemetaphone
+
+# Documentation here: https://github.com/gfairchild/pyxDamerauLevenshtein
+# pip install pyxDamerauLevenshtein
+import pyxdameraulevenshtein
+
+# pip install soundex
+import soundex

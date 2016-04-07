70b4d8b9d60e99b27af17809191957caaba6bf8f
Henry Fisher <henry.wfisher@gmail.com>
2016-04-07 17:41:08 -0500

    Added mostly built phonetics

--- main.py
+++ main.py
@@ -27,3 +27,40 @@ import scikit-learn.org
 #pip install pandas
 import pandas
 
+
+
+# scores_dictionary format:
+# {
+#  "CompleteSquare": 10,
+#  "QuadraticFormula":  30,
+#  ...
+# }
+def phonetics_comparison(user_input, scores_dictionary, keywords_dictionary):
+	# iterate through solving methods
+	# for each one, iterate through scrubbed user input
+	# compare word w/ metaphone to list of keywords for solving method
+	# you get a list of distances from each keyword, reduce list by adding
+	# add this score to scores_dictionary for the method you're using (update, not set)
+
+	# Load phonetics packages
+	dmeta = fuzzy.DMetaphone()
+	soundex = fuzzy.Soundex(4)
+
+	# Split user input into lists for iteration
+	input_list = user_input.split()
+
+	# Iterate through methods for solving, then iterate through words in
+	# scrubbed user input. For each word, compare phonetics to all keywords
+	# and add score to the scores dictionary
+	for method, keywords in keywords_dictionary.iteritems():
+		for word in input_list:
+			metaphone_word = dmeta(word)[0]
+			metaphone_array = map(lambda x: dmeta(x)[0], keywords)
+
+			soundex_word = soundex(word)
+			soundex_array = map(soundex, keywords)
+
+			dist_array = damerau_levenshtein_distance_withNPArray(word, keywords)
+			dist = reduce(lambda x, y: x+y, dist_array)
+
+

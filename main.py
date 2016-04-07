890109a5fca3449e302297173130b252c1fd1548
Tales-of-T <creationofwords@gmail.com>
2016-04-07 14:17:13 -0500

    adding nap packages, ml packages, and read/write data packages
    Added in nltk for stop words converted to a set,  added in scikit-learn
    for potential classification algs for probabilities, added in pandas
    for read/writing

--- main.py
+++ main.py
@@ -5,3 +5,25 @@ import fuzzy
 # Documentation here: https://github.com/gfairchild/pyxDamerauLevenshtein
 # pip install pyxDamerauLevenshtein
 import pyxdameraulevenshtein
+
+
+# Documentation here: http://www.nltk.org/
+# pip install nltk
+import nltk
+nltk.download()  # Download text data sets, including stop words
+from nltk.corpus import stopwords
+# bag of stop words to exclude from input, convert to a set for search
+stop = set(stopwords.words('english'));
+
+# ML Package, Documentation:
+#  http://scikit-learn.org/stable/modules/svm.html#multi-class-classification
+#  http://scikit-learn.org/stable/modules/tree.html#classification
+#  http://scikit-learn.org/stable/supervised_learning.html#supervised-learning
+#pip install scikit-learn.org
+import scikit-learn.org
+
+
+# Import pandas for reading data in/out for data structures from csv's.
+#pip install pandas
+import pandas
+

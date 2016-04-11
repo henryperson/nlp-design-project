24d2bb3a6105d9af06776fc38fcdb48f79f5fd9b
Tales-of-T <creationofwords@gmail.com>
2016-04-11 10:46:08 -0500

    handled input attempts, fixed typing
    Added in a new function called takeUserInput that reads a sentence in,
    processes it, tokenizes it, handles “not x” cases.

--- readInput.py
+++ readInput.py
@@ -11,18 +11,31 @@ from nltk.corpus import stopwords
 def split_upper(s):
     return filter(None, re.split("([A-Z][^A-Z]*)", s))
 
-
 def readInput(keywords_dictionary, file_name):
 	# open file for reading
 	f = open(file_name, 'r')
 	# for each line, set the dictionary right for likelihoods
 	for line in f: 
 		current = line.rstrip()
-		splitWords = map(lambda x:x.lower(), split_upper(current));
-		filtered_words = [word for word in splitWords if word not in stopwords.words('english')]
+		split_words = map(lambda x:x.lower(), split_upper(current));
+		filtered_words = [word for word in split_words if word not in stopwords.words('english')]
 		keywords_dictionary[current] = filtered_words
 
 	return keywords_dictionary
 
+def takeUserInput():
+	solve_method = raw_input("How will you solve this problem? ");
+	split_words = map(lambda x:x.lower(), solve_method.split());
+	stop = stopwords.words('english')
+	stop.remove("not");
+	filtered_words = [word for word in split_words if word not in stop]
+	try: 
+		target_index = filtered_words.index("not")
+	except ValueError, e:
+		target_index = None
+	return filtered_words[:target_index]
 
+# TESTING: 
 #print(readInput({}, "text.csv"))
+#t = takeUserInput()
+#print(t);

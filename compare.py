96c755982f1e8eb83112493446e2e0dd9ff1a8db
Henry Fisher <henry.wfisher@gmail.com>
2016-04-14 19:34:08 -0500

    Stricter threshold for unidentified method

--- compare.py
+++ compare.py
@@ -40,7 +40,7 @@ import math
 #  ...
 # }
 def compare(input_list, keywords_dictionary, word_weights):
-	# print(input_list)
+	print(input_list)
 	# print(word_weights)
 	# Load phonetics functions
 	dmeta = fuzzy.DMetaphone()
